3739c42e3c399b4d2a945dedcd2ba7a3ca1be5a0
awadh <awadh.hassan@student.moringaschool.com>
2022-09-06 23:26:05 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+function submit(name,email){
+return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+return  fetch ("http://localhost:3000/users" ,{
+  method: "POST",
+  headers: {
+    "Content-Type":"application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify({
+    name,
+    email,
+  }),
+})
+  .then(function (response) {
+    return response.json();
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
+  .then(function (object) {
+    document.body.textContent = object["id"];
+  })
+  .catch(function (error) {
+    document.body.textContent = error.message;
+  })
+};
